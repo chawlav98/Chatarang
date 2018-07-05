be11d2a046c2cad21e4271e4bbdf2702dd41269e
Vedant Chawla <chawlav@iu.edu>
2018-07-05 16:56:50 -0400

    Persist user in localStorage

--- src/App.js
+++ src/App.js
@@ -8,24 +8,37 @@ class App extends Component {
   constructor() {
     super()
 
+    const user = JSON.parse(localStorage.getItem('user'))
+
     this.state = {
-      user: {}
+      user: user || {},
     }
   }
-handleAuth = (user) => {
-  this.setState ({user})
-}
 
-signedIn = () => {
-  return this.state.user.uid
-}
+  handleAuth = (user) => {
+    this.setState({ user })
+    localStorage.setItem('user', JSON.stringify(user))
+  }
+
+  signedIn = () => {
+    return this.state.user.uid
+  }
+
+  signOut = () => {
+    this.setState({ user: {} })
+    localStorage.removeItem('user')
+  }
+
   render() {
     return (
       <div className="App">
         {
           this.signedIn()
-            ?   <Main user={this.state.user} />
-            :<SignIn handleAuth = {this.handleAuth} />
+            ? <Main
+                user={this.state.user}
+                signOut={this.signOut}
+              />
+            : <SignIn handleAuth={this.handleAuth} />
         }
       </div>
     )
